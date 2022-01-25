172c6fee733eb107742b369fd86663607db5aca7
Dell Daniel <gordongecco77@gmail.com>
2022-01-25 18:09:16 +0100

    refactor

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
+
+  const onClickLeft =()=>{
+const element = document.getElementsByClassName('image-wrapper')[0];
+element.classList.add('move');
+  };
+  const onClickRight =()=>{
+    const element = document.getElementsByClassName('image-wrapper')[0];
+    element.classList.remove('move');
+  };
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button className='button-left' onClick={onClickLeft}>mozgat</button>
+        <button className='button-right' onClick={onClickRight}>vissza</button>
+
+        <div className='image-wrapper1'>
+      <img src="https://images.unsplash.com/photo-1586227740560-8cf2732c1531?ixlib=rb-1.2.1&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1561&q=80" alt="kep" />
+    </div> 
+    <div className='image-wrapper'>
+      <img src="https://images.unsplash.com/photo-1642986951104-428827cfe46b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80" alt="kep" />
+    </div>
     </div>
   );
 }
