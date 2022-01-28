bc303a0949abe9558d5a4724c087d9f80f451ea8
gordongecco <delldani001@gmail.com>
2022-01-28 09:45:55 +0100

    ref

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,59 @@
 import React from "react";
 import { Slider } from "./Slider";
 
+const images = [
+  {
+    id: "1",
+    src: "https://images.unsplash.com/photo-1643175816971-a463dee6ae61?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+  },
+  {
+    id: "2",
+    src: "https://images.unsplash.com/photo-1586227740560-8cf2732c1531?ixlib=rb-1.2.1&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1561&q=80",
+  },
+  {
+    id: "3",
+    src: "https://images.unsplash.com/photo-1642986951104-428827cfe46b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+  },
+  {
+    id: "4",
+    src: "https://images.unsplash.com/photo-1643133277936-9f93d8792522?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+  },
+  {
+    id: "5",
+    src: "https://images.unsplash.com/photo-1642977129002-410dec0c90c5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+  },
+  {
+    id: "6",
+    src: "https://images.unsplash.com/photo-1643146970682-7edef5a299ec?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1171&q=80",
+  },
+  {
+    id: "7",
+    src: "https://images.unsplash.com/photo-1643139713458-1e3e31577d38?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+  },
+  {
+    id: "8",
+    src: "https://images.unsplash.com/photo-1643118156795-260d71d95756?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1076&q=80",
+  },
+  {
+    id: "9",
+    src: "https://images.unsplash.com/photo-1643145024647-9d13e5185dd1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1172&q=80",
+  },
+  {
+    id: "10",
+    src: "https://images.unsplash.com/photo-1643130420763-964c891d218c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1145&q=80",
+  },
+];
+
 const App = () => {
-  return <Slider />;
+  const cards: JSX.Element[] = images.map((item, index) => {
+    return (
+      <div style={{ width: "100%", height: "100%" }} key={index}>
+        <img src={item.src} alt={index.toString()} />
+      </div>
+    );
+  });
+
+  return <Slider images={cards} />;
 };
 
 export default App;
